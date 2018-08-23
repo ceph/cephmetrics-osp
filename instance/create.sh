a4d4d4378be53fdcf2aeef9085fc5a35d1fc56ca
Zack Cerza <zack@redhat.com>
2018-08-23 16:14:23 -0700

    Set up the second NIC via a user-data hack
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- instance/create.sh
+++ instance/create.sh
@@ -89,13 +89,33 @@ PRIV_PORT_ID=$(openstack port list --network $PRIV_NET_ID --tags $PRIV_PORT_NAME
 openstack keypair create $KEYPAIR_NAME > ~/$KEYPAIR_NAME.pem
 chmod 600 ~/$KEYPAIR_NAME.pem
 
+# Assemble the user-data
+# This is necessary because up until cloud-init 18.3, network configuration
+# data sent by OpenStack wasn't used. This meant that only one NIC was usable.
+# RHEL 7.6 is slated to ship 18.2.
+# https://github.com/cloud-init/cloud-init/commit/cd1de5f
+# https://bugs.launchpad.net/cloud-init/+bug/1749717
+cat << USER_DATA > /tmp/user-data.txt
+#!/usr/bin/env bash
+cat << IFCFG > /etc/sysconfig/network-scripts/ifcfg-eth1
+DEVICE="eth1"
+ONBOOT=yes
+HWADDR=$(openstack port list --tags $PROVIDER_PORT_NAME -c "MAC Address" -f value)
+TYPE=Ethernet
+BOOTPROTO=static
+IPADDR=$PROVIDER_NET_IP
+$(ipcalc --netmask $PROVIDER_NET_CIDR)
+IFCFG
+ifup eth1
+USER_DATA
+
 openstack server create --flavor $FLAVOR_NAME --image $IMAGE_NAME \
                         --key-name $KEYPAIR_NAME \
                         --nic port-id=$PRIV_PORT_ID \
                         --nic port-id=$PROVIDER_PORT_ID \
                         --security-group $SEC_GROUP_NAME \
+                        --user-data /tmp/user-data.txt \
                         --wait $SERVER_NAME
-#PRIV_PORT_ID=$(openstack port list --server $SERVER_NAME --network $PRIV_NET_ID --tags $PRIV_PORT_NAME -c id -f value)
 
 openstack floating ip create --port $PRIV_PORT_ID --floating-ip-address $FLOAT_IP $PUB_NET_NAME
 
